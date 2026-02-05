f58620ee00aa48a9cc104078c0c190b192d7d0cc
lk_07 <lukakrzan007@gmail.com>
2026-02-05 19:52:00 +0100

    zacetno premikanje kace

--- Snake_pygame.py
+++ Snake_pygame.py
@@ -18,9 +18,15 @@ glava = pygame.Rect(400, 300, 25, 25)
 jabolko = pygame.Rect(random.randint(50, 750), random.randint(50, 550), 13, 13)
 
 barve = [(0, 0, 0),(0, 128, 0), (210, 4, 45)]
+hitrost = (3, 0)
 
 while not exit:
     canvas.fill(color)
+    clock.tick(20)
+    #casova_razlika = clock.tick(60)/100
+
+    glava.x += hitrost[0]
+    glava.y += hitrost[1]
 
 
     pygame.draw.rect(canvas, barve[0] , glava)
@@ -29,5 +35,18 @@ while not exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                hitrost = (3, 0)
+
+            if event.key == pygame.K_a:
+                hitrost = (-3, 0)
+
+            if event.key == pygame.K_w:
+                hitrost = (0, -3)
+
+            if event.key == pygame.K_s:
+                hitrost = (0, 3)
 
     pygame.display.update()
